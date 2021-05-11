4370836ff2dbecf712d02e5c89f572c7fb99f9cc
Seif <seif@>
2021-05-11 16:15:32 +0200

    Aeroport + test

--- tp-vol/tp-vol-dao/src/main/java/sopra/vol/Application.java
+++ tp-vol/tp-vol-dao/src/main/java/sopra/vol/Application.java
@@ -4,8 +4,17 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import sopra.vol.dao.IAeroportDao;
+import sopra.vol.dao.IVilleDao;
+import sopra.vol.dao.jdbc.AeroportDaoJdbc;
+import sopra.vol.dao.jdbc.VilleDaoJdbc;
+
 public class Application {
 	private static Application instance = null;
+	
+	
+	private final IVilleDao villeDao = new VilleDaoJdbc();
+	private final IAeroportDao aeroportDao = new AeroportDaoJdbc();
 
 	private final String jdbcUrl = "jdbc:mysql://localhost:3306/tp_vol";
 	private final String username = "root";
@@ -26,6 +35,10 @@ public class Application {
 		}
 
 		return instance;
+	}
+	
+	public IVilleDao getVilleDao() {
+		return villeDao;
 	}
 
 	public Connection getConnection() throws SQLException {
